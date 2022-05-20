3f69bbbb22c7324d77d9ef5a752c9a89237674fd
lotyp <lotyp7@gmail.com>
2022-05-20 15:55:11 +0300

    ci: removing poetry from macos

---
+++ tests/gettext-symlinks.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+### /usr/local/bin
+ln -s /usr/local/gettext/0_20_1/bin/autopoint /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/envsubst /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/gettext /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/gettextize /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/gettext.sh /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgattrib /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgcat /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgcmp /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgcomm /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgconv /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgen /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgexec /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgfilter /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgfmt /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msggrep /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msginit /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgmerge /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msgunfmt /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/msguniq /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/ngettext /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/recode-sr-latin /usr/local/bin/
+ln -s /usr/local/gettext/0_20_1/bin/xgettext /usr/local/bin/
+
+### /usr/local/include
+ln -s /usr/local/gettext/0_20_1/include/autosprintf.h /usr/local/include/
+ln -s /usr/local/gettext/0_20_1/include/gettext-po.h /usr/local/include/
+ln -s /usr/local/gettext/0_20_1/include/textstyle /usr/local/include/
+ln -s /usr/local/gettext/0_20_1/include/textstyle.h /usr/local/include/
+
+### /usr/local/lib
+ln -s /usr/local/gettext/0_20_1/lib/gettext /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libasprintf.a /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libasprintf.la /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libasprintf.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libasprintf.so.0 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libasprintf.so.0.0.0 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextlib-0.20.1.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextlib.la /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextlib.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextpo.a /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextpo.la /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextpo.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextpo.so.0 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextpo.so.0.5.5 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextsrc-0.20.1.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextsrc.la /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libgettextsrc.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libtextstyle.a /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libtextstyle.la /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libtextstyle.so /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libtextstyle.so.0 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/libtextstyle.so.0.0.0 /usr/local/lib/
+ln -s /usr/local/gettext/0_20_1/lib/preloadable_libintl.so /usr/local/lib/
+
+### /usr/local/share/aclocal
+ln -s /usr/local/gettext/0_20_1/share/aclocal/gettext.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/host-cpu-c-abi.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/iconv.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/intlmacosx.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/lib-ld.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/lib-link.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/lib-prefix.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/nls.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/po.m4 /usr/local/share/aclocal/
+ln -s /usr/local/gettext/0_20_1/share/aclocal/progtest.m4 /usr/local/share/aclocal/
