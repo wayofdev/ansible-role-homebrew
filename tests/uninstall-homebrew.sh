3554a6b89385eb2b754e41b92558bcdd5e35b1fd
lotyp <lotyp7@gmail.com>
2022-06-24 04:50:45 +0300

    ci: run macos tests without molecule

--- tests/uninstall-homebrew.sh
+++ tests/uninstall-homebrew.sh
@@ -11,3 +11,18 @@ sudo ./uninstall.sh --force
 sudo rm -rf /usr/local/Homebrew
 sudo rm -rf /usr/local/Caskroom
 sudo rm -rf /usr/local/bin/brew
+
+# Additional directories
+# sudo rm -rf /usr/local/Frameworks
+# sudo rm -rf /usr/local/aws-cli
+# sudo rm -rf /usr/local/bin
+# sudo rm -rf /usr/local/etc
+# sudo rm -rf /usr/local/include
+# sudo rm -rf /usr/local/lib
+# sudo rm -rf /usr/local/microsoft
+# sudo rm -rf /usr/local/miniconda
+# sudo rm -rf /usr/local/opt
+# sudo rm -rf /usr/local/sbin
+# sudo rm -rf /usr/local/sessionmanagerplugin
+# sudo rm -rf /usr/local/share
+# sudo rm -rf /usr/local/var
