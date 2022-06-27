dedf19db401629d6cebb6fa235d96e94d6aaf512
lotyp <lotyp7@gmail.com>
2022-06-28 00:52:43 +0300

    feat: adding ssh debug

--- .github/scripts/uninstall-homebrew.sh
+++ .github/scripts/uninstall-homebrew.sh
@@ -3,9 +3,10 @@
 # Uninstalls Homebrew using the official uninstallation script.
 
 ### Download and run the uninstallation script.
-curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
-chmod +x ./uninstall.sh
-sudo ./uninstall.sh --force
+# curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
+# chmod +x ./uninstall.sh
+# sudo ./uninstall.sh --force
+sudo ../../contrib/homebrew-install/uninstall.sh --force
 
 ### Clean up Homebrew directories.
 sudo rm -rf /usr/local/Homebrew
