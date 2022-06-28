a09aefd0ecf598368a6de6d5eb63fd134b87c007
lotyp <lotyp7@gmail.com>
2022-06-28 04:01:50 +0300

    feat: separate analytics

--- .github/scripts/uninstall-homebrew.sh
+++ .github/scripts/uninstall-homebrew.sh
@@ -16,13 +16,13 @@ sudo rm -rf /usr/local/bin/brew
 ### Additional directories
 # sudo rm -rf /usr/local/Frameworks
 # sudo rm -rf /usr/local/bin
-sudo rm -rf /usr/local/etc
-sudo rm -rf /usr/local/include
-sudo rm -rf /usr/local/lib
-sudo rm -rf /usr/local/opt
+# sudo rm -rf /usr/local/etc
+# sudo rm -rf /usr/local/include
+# sudo rm -rf /usr/local/lib
+# sudo rm -rf /usr/local/opt
 # sudo rm -rf /usr/local/sbin
-sudo rm -rf /usr/local/share
-sudo rm -rf /usr/local/var
+# sudo rm -rf /usr/local/share
+# sudo rm -rf /usr/local/var
 
 ### Github runner specific directories
 # sudo rm -rf /usr/local/sessionmanagerplugin
